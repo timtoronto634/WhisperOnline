e0b389b0956cedde2d7db557b4505ae1b22424a9
timtoronto634 <tim225x@gmail.com>
2023-03-17 01:04:49 +0900

    Feat add chatting of chatgpt

--- transcribe.py
+++ transcribe.py
@@ -1,5 +1,7 @@
 import openai
 import os
+import time
+import datetime
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -7,17 +9,38 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 import pyaudio
 import wave
 
+def chat(text):
+  resp = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=text,
+    max_tokens=2048,
+    temperature=0.2
+  )
 
-def main():
+  print("ChatGPT Response:")
+  if resp.choices:
+    for choice in resp.choices:
+        if choice.text:
+            print(choice.text.strip())
+
+  print("---")
+  print(f"total_tokens used: {resp.usage.total_tokens}")
+  return
+
+def transcribe():
   chunk = 1024
   format_audio = pyaudio.paInt16
   channels = 1
   fs = 44100
   seconds = 5
-  filename = "input.wav"
+  dir_name = "data"
+  if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+  filename = f"{dir_name}/{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.wav"
 
   audio = pyaudio.PyAudio()
 
+  print("you can now speak")
   stream = audio.open(
       format=format_audio,
       channels=channels,
@@ -35,6 +58,7 @@ def main():
   stream.stop_stream()
   stream.close()
   audio.terminate()
+  print("finished recording")
 
   # Write audio file to disk in .wav format
   with wave.open(filename, 'wb') as wave_file:
@@ -43,9 +67,20 @@ def main():
     wave_file.setframerate(fs)
     wave_file.writeframes(b''.join(frames))
 
+  text = ""
   with open(filename, 'rb') as audio_file:
     response = openai.Audio.transcribe("whisper-1", audio_file)
     print(response.text)
+    text = response.text
+  return text
+
+
+def main():
+  for _ in range(5):
+    text = transcribe()
+    chat(text)
+    time.sleep(5)
+
 
 if __name__ == "__main__":
   main()
